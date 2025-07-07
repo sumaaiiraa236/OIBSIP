e5c15ddebbc391ebc2acac253fa823af0ba858fa
sumaaiiraa236 <ahmedsumairaa@gmail.com>
2025-07-07 21:26:25 +0530

    final commit for BMI calculator

--- bmi.py
+++ bmi.py
@@ -1,17 +1,17 @@
 def calculatebmi(weight,height):
     return weight/(height**2)
-def bmimeter(indx):
-    if indx < 18.5:
+def bmimeter(bmi):
+    if bmi < 18.5:
         return "underweight"
-    elif 18.5<=indx <24.5:
+    elif 18.5<=bmi<24.5:
         return "normal body mass index"
-    elif indx>24.5:
+    elif bmi>24.5:
         return "overweight"
     else:
         return "obese"
 print(f"WELCOME TO THE BMI CALCULATOR!") 
-weight=float(input("enter the wieght: "))
-height=float(input("enter the height: "))
+weight=float(input("enter the wieght in kgs: "))
+height=float(input("enter the height in m: "))
 bmi=calculatebmi(weight,height)
-meter=bmimeter(indx)
-print(f"enter the bmi {bmi:.2f} ({meter})")
+meter=bmimeter(bmi)
+print(f"your the bmi {bmi:.2f} ({meter})")
