4cdf9bacebfe4e208545591c6232b5c64362ab2b
Sumairaa Ahmed <ahmedsumairaa@gmail.com>
2025-08-14 15:54:15 +0530

    new change

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import threading
+
 def receive_messages(client):
+    """Listen for incoming messages from the server"""
     while True:
         try:
             msg = client.recv(1024).decode()
@@ -9,10 +11,12 @@ def receive_messages(client):
             print(f"Server: {msg}")
         except:
             break
+
+print("[CLIENT] Connecting to server...")  
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 5555))
+print("[CLIENT] Connected to server")    
 threading.Thread(target=receive_messages, args=(client,), daemon=True).start()
-
 while True:
     msg = input()
     client.send(msg.encode())
